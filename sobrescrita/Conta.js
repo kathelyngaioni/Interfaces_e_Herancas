f535d85393fdf072db4bdde0d368cd2118eb9f6f
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-01-19 18:32:04 +0000

    sobrescrita_privado_e_protegido

--- sobrescrita/Conta.js
+++ sobrescrita/Conta.js
@@ -1,10 +1,10 @@
 export class Conta {
   
   constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial;
-        this._cliente = cliente;
-        this._agencia = agencia;
-    }
+    this._saldo = saldoInicial;
+    this._cliente = cliente;
+    this._agencia = agencia;
+  }
 
   set cliente (cliente){
     if(cliente instanceof Cliente){
@@ -12,36 +12,42 @@ export class Conta {
     }
   }
   
-
   get cliente(){
     return this._cliente;
   }
+  
   get saldo(){
     return this._saldo;
   }
 
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
+  sacar(valor) {
+    let taxa = 1;
+    return this._sacar(valor, taxa);
+  }
 
-    depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    tranferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-
-    }
-
-    teste(){
-     console.log("teste na Conta");
+   _sacar(valor, taxa) {
+     const valorSacado = taxa * valor;
+     if (this._saldo >= valor) {
+       this._saldo -= valorSacado;
+       return valorSacado;
+     }
+    // para casos de saldo insuficiente
+    return 0;
    }
+
+  depositar(valor) {
+    if (valor <= 100) {
+      return;
+    }
+    this._saldo += valor;
+  }
+
+  tranferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
+
+  teste(){
+    console.log("teste na Conta");
+  }
 }
--- sobrescrita/ContaCorrente.js
+++ sobrescrita/ContaCorrente.js
@@ -4,18 +4,17 @@ export class contaCorrente extends Conta{
   static numeroDeContas = 0;
   constructor(cliente, agencia) {
     //Conta: saldoinicial, cliente, agencia
-    super(0, cliente, agencia);
-    contaCorrente.numeroDeContas += 1;
+  super(0, cliente, agencia);
+  contaCorrente.numeroDeContas += 1;
   }
-  sacar(valor){
-        let taxa = 1.1;
-        const valorSacado = taxa * valor;
-        if (this._saldo >= valorSacado){
-            this._saldo -= valorSacado;
-            return valorSacado;
-        }
-    }
 
+  //sobrescrevendo o comportamento de sacar
+  sacar(valor) {
+    let taxa = 1.1;
+    // a classe contaCorrente está acessando diretamente um método privado (_sacar(valor,taxa)) da classe pai Conta
+    return this._sacar(valor, taxa);
+  }
+  
   teste(){
     super.teste();
     console.log("teste na contaCorrente");
