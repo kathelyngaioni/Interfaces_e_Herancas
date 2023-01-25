ffd89897d1cd76902022ed50e5d68d6d88fdde6b
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-01-25 19:20:54 +0000

    duck-type

--- sistema_interno/SistemaAutenticacao.js
+++ sistema_interno/SistemaAutenticacao.js
@@ -1,6 +1,17 @@
-//♥ A herança não é a única forma da gente compartilhar código.
-export class SistemaAutenticacao {
-  static login(autenticavel, senha){
-    return autenticavel.autenticar(senha);
-  }
+/*♥ A herança não é a única forma da gente compartilhar código.
+Duck type são comuns em linguagens que não são fortemente tipadas
+*/
+
+export class SistemaAutenticacao{
+    static login(autenticavel, senha){
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+static ehAutenticavel(autenticavel){
+  return "autenticar" in autenticavel &&
+  autenticavel.autenticar instanceof Function
+    }
 }
--- sistema_interno/index.js
+++ sistema_interno/index.js
@@ -10,7 +10,6 @@ const cliente= new Cliente("Lais",123456789,"456");
 
 diretor.cadastrarSenha("123456789");
 gerente.cadastrarSenha("1234");
-//cliente.cadastrarSenha("456");
 
 //o SistemaAutenticacao vai ser um método estatico
 // o erro é porque a classe Cliente não possui o método de autenticar
